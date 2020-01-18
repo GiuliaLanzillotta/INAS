b18eea637b6d524b9db3cbdf7368adbeb0918d43
AkbarShah96 <shahak@student.ethz.ch>
2020-01-18 01:45:33 +0100

    Changes and Comments!

--- src/cnn.py
+++ src/cnn.py
@@ -1,10 +1,7 @@
-""" This module has to build the child architecture
+""" This module has to manage the child architecture
 and save the current architecture
     It has to implement:
     - build_child_arch(action, previous_state)
-    - check_states
-    -update_size
-    -get_reward
 """
 import torch
 from torch import nn
@@ -14,16 +11,16 @@ import numpy as np
 
 max_layers = 15
 
-"Set device to gpu if available otherwise cpu"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class cnn():
 
-    def __init__(self, max_layers, image_size, prev_channels, num_classes, train, epochs=10):
-
+    def __init__(self, max_layers, image_size, prev_channels, num_classes,train, epochs=15):
+        # The initial state is set to be a good eanough architecture in order to
+        # boost the learning
         initial_state = [3, 1, 32, 2, 2,
-                         3, 1, 32, 2, 2,
+                         3, 1, 32, 0, 2,
                          3, 1, 64, 2, 2,
                          3, 1, 64, 2, 2,
                          3, 1, 64, 0, 2,
@@ -32,133 +29,168 @@ class cnn():
                          3, 1, 128, 2, 2,
                          3, 1, 128, 0, 2,
                          3, 1, 128, 2, 2,
-                         3, 1, 128, 0, 2,
+                         3, 1, 128, 2, 2,
                          3, 1, 128, 2, 2,
                          3, 1, 128, 0, 2,
                          3, 1, 128, 2, 2,
-                         3, 1, 128, 2, 2]
+                         3, 1, 128, 0, 2]
         self.state = initial_state
-        self.train = train
         self.image_size = image_size
         self.original_image_size = image_size
         self.prev_channels = prev_channels
         self.num_classes = num_classes
-        self.max_layers= max_layers
-        self.op_add = [lambda x: x+1 , lambda x: x, lambda x: x-1]
-        self.op_mul = [lambda x: x*2, lambda x: x, lambda x: x/2]
+        self.max_layers = max_layers
+        # We define the operations that correspond to the different actions
+        self.op_add = [lambda x: x + 1, lambda x: x, lambda x: x - 1]
+        self.op_mul = [lambda x: x * 2, lambda x: x, lambda x: x / 2]
         self.epochs = epochs
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        return
+        self.train = train
 
-    """Get the padding"""
-    @staticmethod
-    def get_padding(image_size, kernel_size, stride):
-        return np.ceil(((kernel_size - 1) * image_size - stride + kernel_size) / 2)
-
-    """Update_size fixes the image size in order to carry out the relevant check for each layer!"""
     @staticmethod
     def update_size(image_size, kernel_size, stride, padding):
         return int((image_size - kernel_size + 2 * padding) / stride + 1)
 
-    """The build_child_arch generates the CNN by taking the action and applying it to the state, the state contains
-    the hyperparameters of the CNN architecture. """
+    @staticmethod
+    def get_padding(image_size, kernel_size, stride):
+        """Calculated the padding with SAME rule"""
+        return np.ceil(((kernel_size - 1) * image_size - stride + kernel_size) / 2)
+
+    def update_image_size(self, state):
+        n = self.image_size
+        # state[3] contains information about pooling
+        # pooling =0 is max_poos, 1 is avg_pool and 2 is no_pool
+        if state[3] == 2:
+            return n
+        k = state[4]  # filter_size
+        s = 1  # stride
+        return (n - k) / s + 1
+
     def build_child_arch(self, action):
+        """This function does not actually build the child architecture,
+        since that is a responsibility of the conv_net class, but it
+        translates the action in order to modify the current state."""
         state = []
-        self.image_size=self.original_image_size
+        self.image_size = self.original_image_size
+        # Un-rolling the actions state by state
         for layer in range(self.max_layers):
-            action0 = action[0+layer*5]
-            action1 = action[1+layer*5]
-            action2 = action[2+layer*5]
-            action3 = action[3+layer*5]
-            action4 = action[4+layer*5]
-            state0 = self.op_add[action0](self.state[0+layer*5])   # Filter Size, +1, 0, -1
-            state1 = self.op_add[action1](self.state[1+layer*5])   # Stride, +1, 0, -1
-            state2 = self.op_mul[action2](self.state[2+layer*5])   # Number of Channels, *2, 0, /2
-            state3 = action3                                       # Pooling, Yes or No?
-            state4 = self.op_add[action4](self.state[4+layer*5])   # Pooling Size, +1, 0, -1
-            layer_state = [state0,state1,state2,state3,state4]
+            action0 = action[0 + layer * 5]
+            action1 = action[1 + layer * 5]
+            action2 = action[2 + layer * 5]
+            action3 = action[3 + layer * 5]
+            action4 = action[4 + layer * 5]
+            # Calculating the value of the new hyper-parameters
+            state0 = self.op_add[action0](self.state[0 + layer * 5])
+            state1 = self.op_add[action1](self.state[1 + layer * 5])
+            state2 = self.op_mul[action2](self.state[2 + layer * 5])
+            state3 = action3
+            state4 = self.op_add[action4](self.state[4 + layer * 5])
+            # Calculation the value of the new layer
+            layer_state = [state0, state1, state2, state3, state4]
+            # Checking the admissibility of the layer state and
+            # correcting it where necessary
             layer_state = self.check_state(layer_state, layer)
             state.extend(layer_state)
+            # Updating the image size
             padding = self.get_padding(self.image_size, state[0], state[1])
             self.image_size = self.update_size(self.image_size, state[0], state[1], padding)
-            if state[3] != 2:   #If we have pooling then update image size for checks.
+            if state[3] != 2:
                 self.image_size = self.update_size(self.image_size, state[4], 1, 0)
-
-        self.net = conv_net(state, input_size=self.original_image_size, prev_channels = self.prev_channels, n_class=self.num_classes,device = self.device)
+        # Building the actual child architecture
+        self.net = conv_net(state, input_size=self.original_image_size, prev_channels=self.prev_channels,
+                            n_class=self.num_classes, device=self.device)
         self.net = self.net.to(device)
-
+        # Updating and returning the new state
         self.state = state
         return self.state
 
-    """The check_state function ensures the that the state does not violate certain bounds
-       which would not make sense, i.e. stride = 0, more explanation in section 3.3 of the report"""
+    "This function is used to build the best CNN state into an architecture!"
+    def bestCNN(self,state):
+        self.original_image_size = 32
+        self.prev_channels = 3
+        self.num_classes = 10
+
+        self.net = conv_net(state, input_size=self.original_image_size, prev_channels=self.prev_channels,
+                            n_class=self.num_classes, device=self.device)
+        self.net = self.net.to(device)
+
     def check_state(self, state, layer):
-        padding = np.ceil(((state[1]-1)*self.image_size - state[1] + state[0])/2)
+        """This function plays a central role in the training because
+        it sets the bound of admissibility of solution. We tried to keep the bounds
+        as loose as possible, for our computing capabilities. In case any
+        of the parameters exceeds the boundaries, we set them to be equal
+        to a fixed value (1 for filter size, 1 for stride, and No pooling
+        for the pooling)"""
 
-        "0:size of filter, 1:stride, 2:channels, 3:maxpool(boolean), 4:max_pool_size"
+        padding = self.get_padding(self.image_size, state[0], state[1])
+        # State[0] is the filter size.
+        if (state[0] < 1 or state[0] > self.image_size):
+            old = self.state[layer * 5]
+            if (old > self.image_size):
+                state[0] = 1
+            else:
+                state[0] = old
 
-        if (state[0]<1 or state[0]>self.image_size):
-            state[0] = self.state[0+layer*5]
+        # State[1] is the stride.
+        if (state[1] < 1 or state[1] > self.image_size + padding - state[0]):
+            old = self.state[1 + layer * 5]
+            if (old > self.image_size):
+                state[1] = 1
+            else:
+                state[1] = old
 
-        if (state[1]<1 or state[1]>self.image_size + padding - state[0]):
-            state[1] = self.state[1+layer*5]
+        # State[2] is the number of channels.
+        if (state[2] < 1 or state[2] > 128):
+            state[2] = self.state[2 + layer * 5]
 
-        if (state[2]<1 or state[2] > 128):
-            state[2] = self.state[2+layer*5]
+        # reducing image size after convolution
+        padding = self.get_padding(self.image_size, state[0], state[1])
+        image_size = self.update_size(self.image_size, state[0], state[1], padding)
+        # State[4] is the pooling size.
+        if (state[4] < 1 or state[4] >= image_size):
+            old = self.state[4 + layer * 5]
+            if (old > image_size):
+                state[3] = 2
+            else:
+                state[4] = old
 
-        if (state[4]<1 or state[4] >= self.image_size):
-            state[4] = self.state[4+layer*5]
 
         return state
 
-
-    """The get_reward function trains the CNN architecture sampled, 
-      and produces a reward for the REINFORCE Algorithm"""
     def get_reward(self, data_loader):
+        """This function trains the net and test it on the test set to receive
+        an accuracy that will be used as a reward to train the controller"""
         data_loader_train, data_loader_test = data_loader
-        "Define Optimizer, Schedular, and Loss Function"
         criterion = nn.CrossEntropyLoss()
         if self.train == True:
-            optimizer = optim.SGD(self.net.parameters(), lr=0.005, momentum=0.9, nesterov=True)
-            schedular = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.85)
-        else:
+            optimizer = optim.SGD(self.net.parameters(), lr=0.005, weight_decay=0.0005, momentum=0.9, nesterov=True)
+            schedular = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.8)
+        if self.train == False:
             optimizer = optim.SGD(self.net.parameters(), lr=0.005, momentum=0.9, nesterov=True)
             schedular = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.8)
 
-        "Main Training Loop"
-        for epoch in range(self.epochs):
-            "Reset Loss for every epoch"
+        for epoch in range(self.epochs):  # loop over the dataset multiple times
             running_loss = 0.0
-
-            for i, data in enumerate(data_loader_train,0):
-                "Configure Inputs and sent to GPU if available"
+            for i, data in enumerate(data_loader_train, 0):
+                # get the inputs; data is a list of [inputs, labels]
                 inputs, labels = data
                 inputs, labels = inputs.to(device), labels.to(device)
-        
-                "Optimizer Gradient Reset"
+                # zero the parameter gradients
                 optimizer.zero_grad()
-        
-                "forward + loss + backward + optimize"
+                # forward + backward + optimize
                 outputs = self.net(inputs)
-
                 loss = criterion(outputs, labels)
-
                 loss.backward()
-
                 optimizer.step()
-
-                "Print Statistics"
                 running_loss += loss.item()
                 if i % 300 == 299:
                     print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 300))
                     running_loss = 0.0
-                "Train on 51,200 samples then break"
                 if i % 800 == 799:
                     break
             schedular.step()
+        print('Finished Training')
 
-        print('Finished Training CNN')
-        "Calculate Accuracy of CNN on Validation Set"
         correct = 0
         total = 0
         with torch.no_grad():
@@ -169,7 +201,6 @@ class cnn():
                 _, predicted = torch.max(outputs, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
-        "Compute Reward: Reward is the validation accuracy cubed, reward is cubed in the controller update policy!"
+        # Calculate the accuracy as the percentage of correct guesses
         reward = correct / total
         return reward
-
